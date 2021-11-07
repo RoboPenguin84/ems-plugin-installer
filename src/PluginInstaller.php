987ac5f0f79f319c2a46fccc07393a8983359917
RoboPenguin84 <33846412+RoboPenguin84@users.noreply.github.com>
2021-11-07 16:51:33 +0200

    FIX | Wrong implement method

--- src/PluginInstaller.php
+++ src/PluginInstaller.php
@@ -6,11 +6,23 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class PluginInstaller implements Plugin
+class PluginInstaller implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->removeInstaller($installer);
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // $installer = new Installer($io, $composer);
+        // $composer->getInstallationManager()->uninstall($composer->getRepositoryManager()->getLocalRepository(), $installer->getUnis);
     }
 }
